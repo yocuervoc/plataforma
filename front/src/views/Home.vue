5f8327a4bf52aad29991c2f4cced816ae258b1d7
yocuervoc <yocuervoc@unal.edu.co>
2020-02-27 23:40:08 -0500

    tabla

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -8,6 +8,25 @@
     
     <img src="../assets/universidad2.jpeg" class="imagen">
     <h1 class="titulo">Software para la caracterizacion de la deserción y la inclusión D&I</h1>
+
+        <table class="table" :class="tabla">
+        
+        <tr>
+            <td><router-link to="/ver">1.inicio</router-link></td>
+            <td><router-link to="/ver">2.Registro</router-link></td>
+            <td><router-link to="/ver">3.ver estudiantes</router-link></td>
+            <td><router-link to="/ver">4.Realizar Encuesta</router-link></td>
+        </tr>
+        <tr>
+            <td><router-link to="/ver">5.Alarmas I&D</router-link></td>
+            <td><router-link to="/ver">6.Curso D&</router-link></td>
+            <td><router-link to="/ver">7.reportes</router-link></td>
+            <td><router-link to="/ver">8.Ayuda</router-link></td>
+        </tr>
+
+    </table>
+
+    
     <p class="bienvenida">Bienvenido al Software  para la caracterización de la deserción y la inclusión D&I</p>
     <br>
     <p class="terminos">Términos y condiciones de uso del Software  para la caracterización de la 
@@ -38,15 +57,23 @@
 .bienvenida{
   position: absolute;
   margin-left: 52%;
-  margin-top: 27%;
+  margin-top: 4%;
   color:rgb(17, 70, 42);
 }
 .terminos{
   color:rgb(17, 70, 42);
   position: absolute;
-  margin-top: 27%;
+  margin-top: 6%;
   margin-left: 0%;
   padding: 3%;
   text-align: justify
+}
+.tabla {
+  padding-top: 5%;
+  color: rgb(17, 70, 42);
+  position: absolute;
+  margin-top: 27%;
+  margin-left: 0%;
+  
 }
 </style>
